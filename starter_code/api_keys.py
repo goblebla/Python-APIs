0a2756c300c133672637d2ed2308fcf9cf9e31bc
Blaine Gobler <goblerblaine@yahoo.com>
2020-05-09 22:11:29 -0700

    Submitting final API Homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "40449008a54beb2007d8de8d8b5d63a4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBCyoqX-kprrB-siGC6JWUZ872EGSV5jR8"
